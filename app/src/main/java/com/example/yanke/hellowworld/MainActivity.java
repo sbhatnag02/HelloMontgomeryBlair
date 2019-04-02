404c9226e256868ab9287307dce7f302cafc9425
yankees1fan2002 <yankees1fan2002@gmail.com>
2019-04-02 08:21:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yanke/hellowworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yanke.hellowworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
